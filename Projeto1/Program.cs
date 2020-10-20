72756dc40e2550a3d5ccc704653bc98dca5b2925
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-20 10:00:28 -0300

    FileInfo e File

---
+++ Projeto1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //windows
+            //string sourcePath = "c:\\temp\\arquivotexto.txt";
+            //ou
+            //string sourcePath = @"c:\temp\arquivotexto.txt";
+
+            //mac
+            // diretorio padrao '/Users/nxgames/Projects/Solucao1/Projeto1/bin/Debug/netcoreapp3.1/arquivotexto.txt
+
+            string sourcePath = "/Users/nxgames/Projects/Solucao1/Projeto1/arquivotexto.txt";
+            string targetPath = "/Users/nxgames/Projects/Solucao1/Projeto1/arquivotexto2.txt";
+
+            try
+            {
+                //Instanciei arquivo
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                fileInfo.CopyTo(targetPath);
+
+
+                //Nao precisei instanciar, por isso passei o arquivo
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+
+                }
+
+            }
+
+            catch (IOException e )
+            {
+
+                Console.WriteLine("IO ERROR: " + e.Message );
+            }
+
+
+        }
+    }
+}
